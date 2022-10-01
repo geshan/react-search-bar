61f3e274817deb65f768948eefa2a35bcba1f678
Geshan Manandhar <geshan@simplywallst.com>
2022-10-01 21:50:25 +1000

    Adds tests for the HackerNews Stories component

--- src/HackerNewsStories.js
+++ src/HackerNewsStories.js
@@ -6,7 +6,7 @@ const HackerNewsStories = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchStories = async () => {
       try {
         const data = await (await fetch('https://hn.algolia.com/api/v1/search_by_date?tags=front_page&hitsPerPage=20')).json();
         setStories(
@@ -20,7 +20,7 @@ const HackerNewsStories = () => {
         setLoading(false);
       }
     };
-    fetchUsers();
+    fetchStories();
   }, []);
 
   return (
@@ -33,7 +33,7 @@ const HackerNewsStories = () => {
           stories.map(({ objectID, url, title, author, points }) => (
             title && url &&
             <div className='stories-list' key={objectID}>
-              <h3><a href={url} target="_blank" rel="noreferrer">{title}</a> - By {author} ({points} points)</h3>
+              <h3><a href={url} target="_blank" rel="noreferrer">{title}</a> - By <b>{author}</b> ({points} points)</h3>
             </div>                        
           ))}
       </div>
---
+++ src/HackerNewsStories.test.js
@@ -0,0 +1,71 @@
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import HackerNewsStories from './HackerNewsStories';
+
+let windowFetchSpy;
+
+function wait(milliseconds){
+  return new Promise(resolve => {
+      setTimeout(resolve, milliseconds);
+  });
+}
+
+const mockHnResponse = {
+  'hits': [
+    {
+      'created_at': '2022-10-01T20:26:19.000Z',
+      'title': 'TikTok tracks you across the web, even if you don’t use the app',
+      'url': 'https://www.consumerreports.org/electronics-computers/privacy/tiktok-tracks-you-across-the-web-even-if-you-dont-use-app-a4383537813/',
+      'author': 'bubblehack3r',
+      'points': 123,
+      'objectID': '33049774',
+    },
+    {
+      'created_at': '2022-10-01T15:16:02.000Z',
+      'title': 'The self-taught UI/UX designer roadmap (2021)',
+      'url': 'https://bootcamp.uxdesign.cc/the-self-taught-ui-ux-designer-roadmap-in-2021-aa0f5b62cecb',
+      'author': 'homarp',
+      'points': 253,    
+      'objectID': '33047199',
+    },    
+  ],
+  'page': 0,
+  'query': '',
+};
+
+let mockFetch = async (url) => {
+  if (url.startsWith('https://hn.algolia.com/') && url.includes('front_page')) {
+    await wait(70);
+    return {
+      ok: true,
+      status: 200,
+      json: async () => mockHnResponse,
+    };
+  }
+}
+
+
+beforeEach(() => {
+  windowFetchSpy = jest.spyOn(window, 'fetch').mockImplementation(mockFetch);
+})
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+test('should render latest HN stories H2', async () => {
+  render(<HackerNewsStories />);
+  const latestStoriesH2 = await screen.getByText('Latest HN Stories');
+  expect(latestStoriesH2).toBeInTheDocument();
+});
+
+test('should show stories after they are fetched', async () => {
+  render(<HackerNewsStories />);
+  expect(windowFetchSpy).toHaveBeenCalled();
+  expect(windowFetchSpy).toHaveBeenCalledWith('https://hn.algolia.com/api/v1/search_by_date?tags=front_page&hitsPerPage=20');
+
+  //expect(screen.getByText('The self-taught UI/UX designer roadmap (2021)')).toBeInTheDocument(); //this will fail
+  await waitFor(() => {
+    expect(screen.getByText('The self-taught UI/UX designer roadmap (2021)')).toBeInTheDocument();
+    expect(screen.getByText('homarp')).toBeInTheDocument();
+  });  
+});
